fd4eecc43d46db1311db45fc754f157ee9f3875a
Stephen Tan <stephen2502@hotmail.co.uk>
2019-10-15 19:37:45 +1100

    First Actual Commit
    Working on tail physics

---
+++ Hunted/Assets/Scripts/KeyBoardMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    int speed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        if (Input.GetKey("a"))
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+        }
+    }
+}
